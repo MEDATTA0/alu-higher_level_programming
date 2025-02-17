cfb5e33f4065ee2935eb53dc97b4cbc9186b38f9
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-17 13:24:54 +0200

    exercise 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 
 def fizzbuzz():
     for i in range(101):
-        if i % 3 == i % 5:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif not i % 3:
             print("Fizz", end=" ")
