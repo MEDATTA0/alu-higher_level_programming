54ba8efcd7766e5fc19a3466f217fdb89ec0bb50
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-17 10:58:14 +0200

    exercise 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 
-def islower(char: str):
-    return char.upper()
+def uppercase(string: str):
+    upper_string = ""
+    for char in string:
+        if char.islower():
+            upper_string += chr(ord(char) + 32)
+        else:
+            upper_string += char
+    print(upper_string)
