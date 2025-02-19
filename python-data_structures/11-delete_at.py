a5e0d40ceff94772d829f2892952cfd177c8abcf
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-19 23:34:06 +0200

    exercise 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,6 +3,6 @@
 
 def delete_at(my_list: list, idx=0):
     if 0 <= idx < len(my_list):
-        return my_list[:idx] + my_list[idx:]
+        return my_list[:idx] + my_list[idx + 1 :]
     else:
         return my_list
