e2aa459dc79560d988aa3d97e3702860dc9017be
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-19 19:19:45 +0200

    project 4: Data Structures

---
+++ python-data_structures/1-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    for integer in my_list:
+
+        print(str.format(str(integer)))
+
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
