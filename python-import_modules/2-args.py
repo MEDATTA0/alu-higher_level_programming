0426a2d35453d910e8882c3b8f5d06aa1657cf56
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-19 16:01:00 +0200

    exercise2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) == 0:
+    print("0 arguments.")
+else:
+    args = sys.argv
+    i = 0
+    print(f"{len(args)} {"arguments:" if len(args) > 1 else "argument:"}")
+    for arg in args:
+        if i == 0:
+            i += 1
+            continue
+        print(f"{i}: {arg}")
+        i += 1
