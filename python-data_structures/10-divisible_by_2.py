cbbe0be837c5cb46aa7644039c4363920a348f39
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-19 23:58:31 +0200

    exercise 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -6,7 +6,7 @@ def divisible_by_2(my_list: list):
         return None
     bool_list = []
     for number in my_list:
-        if number % 2 == 1:
+        if number % 2 == 0:
             bool_list.append(True)
         else:
             bool_list.append(False)
