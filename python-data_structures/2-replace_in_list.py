b609f773ead0bd469cef4b9fc06052a2130f04e1
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-19 22:23:20 +0200

    exercise 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def element_at(my_list: list, idx: int, new_element: int):
+def replace_in_list(my_list: list, idx: int, new_element: int):
     if 0 <= idx < len(my_list):
         my_list[idx] = new_element
         return my_list
