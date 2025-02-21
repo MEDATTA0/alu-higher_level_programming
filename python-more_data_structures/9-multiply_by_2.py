6822a084c7567679362511dd88c91cbda73364df
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-21 21:56:20 +0200

    exercise 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,7 @@
 
 
 def multiply_by_2(a_dictionary: dict):
+    new_a_dictionary = {}
     for key in a_dictionary.keys():
-        a_dictionary[key] *= 2
-    return a_dictionary
+        new_a_dictionary[key] = a_dictionary[key] * 2
+    return new_a_dictionary
