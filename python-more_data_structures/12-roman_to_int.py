6db66de3cb8340152f19d3a7408de60eac01f1bf
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-21 22:36:53 +0200

    exercise 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,32 +2,35 @@
 
 
 def roman_to_int(roman_number: str) -> int:
-    roman_correspondence = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "XL": 40,
-        "L": 50,
-        "XC": 90,
-        "C": 100,
-        "CD": 400,
-        "D": 500,
-        "CM": 900,
-        "M": 1000,
-    }
+    if isinstance(roman_number, str):
+        roman_correspondence = {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10,
+            "XL": 40,
+            "L": 50,
+            "XC": 90,
+            "C": 100,
+            "CD": 400,
+            "D": 500,
+            "CM": 900,
+            "M": 1000,
+        }
 
-    i = 0
-    digital_number = 0
-    length = len(roman_number)
+        i = 0
+        digital_number = 0
+        length = len(roman_number)
 
-    while i < length:
-        if i + 1 < length and roman_number[i : i + 2] in roman_correspondence:
-            digital_number += roman_correspondence[roman_number[i : i + 2]]
-            i += 2
-        else:
-            digital_number += roman_correspondence[roman_number[i]]
-            i += 1
+        while i < length:
+            if i + 1 < length and roman_number[i : i + 2] in roman_correspondence:
+                digital_number += roman_correspondence[roman_number[i : i + 2]]
+                i += 2
+            else:
+                digital_number += roman_correspondence[roman_number[i]]
+                i += 1
 
-    return digital_number
+        return digital_number
+    else:
+        return 0
