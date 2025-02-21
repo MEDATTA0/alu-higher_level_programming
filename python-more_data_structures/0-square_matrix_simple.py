373ed287eac65795cfbf32084e48b88ba4e1babe
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-21 14:38:27 +0200

    exercise 1

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
 
-    def square_matrix_simple(matrix: list):
-        return [[x**2 for x in row] for row in matrix]
+def square_matrix_simple(matrix: list):
+    return [[x**2 for x in row] for row in matrix]
