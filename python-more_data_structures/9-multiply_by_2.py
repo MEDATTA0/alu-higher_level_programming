4c4f5c5167333f4bd03a9237d2d73d78d85a7321
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-21 21:53:14 +0200

    exercise 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,6 +5,3 @@ def multiply_by_2(a_dictionary: dict):
     for key in a_dictionary.keys():
         a_dictionary[key] *= 2
     return a_dictionary
-
-
-print(multiply_by_2({"John": 12, "Alex": 8, "Bob": 14, "Mike": 14, "Molly": 16}))
