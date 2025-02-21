00d827044720be562710f1680e9cdda85d763dd0
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-21 14:37:32 +0200

    exercise 1

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,14 +3,4 @@
 if __name__ == "__main__":
 
     def square_matrix_simple(matrix: list):
-        new_matrix = []
-        for i in matrix:
-            if isinstance(i, list):
-                temporary_matrix = []
-                for j in i:
-                    temporary_matrix.append(j**2)
-                new_matrix.append(temporary_matrix)
-            else:
-                new_matrix.append(i**2)
-
-        return new_matrix
+        return [[x**2 for x in row] for row in matrix]
