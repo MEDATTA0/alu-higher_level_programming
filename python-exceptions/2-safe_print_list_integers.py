9cffc683b22c043953aabf8ed130fca273008861
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-02-23 09:34:13 +0200

    exercise 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list: list, x: int):
                 count += 1
             except (ValueError, TypeError):
                 pass
-    except IndexError:
-        pass
+    except IndexError as index_err:
+        return index_err
     print()
     return count
