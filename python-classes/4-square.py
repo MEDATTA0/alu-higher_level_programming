d4d18a261b7af1ca798483c209059dc343f53bd9
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-03-04 13:23:05 +0200

    exercise 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,23 +9,22 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the square with a private instance attribute size."""
+        self.__size = size
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        pass
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.__size
 
-    def size(self, value: int):
+    @size.setter
+    def size(self, value):
+        """Setter for size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-        pass
+        self.__size = value
 
     def area(self):
+        """Returns the area of the square."""
         return self.__size**2
