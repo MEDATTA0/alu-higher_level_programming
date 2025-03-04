215247aed4070ee68f72c7eec46538836c797289
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-03-04 13:07:38 +0200

    exercise 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,15 +10,10 @@ class Square:
     def __init__(self, size=0):
         """Initializes the square with a private instance attribute size."""
 
-        try:
-            if not isinstance(size, int):
-                raise TypeError()
-            elif size < 0:
-                raise ValueError()
-            else:
-                self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
         pass
