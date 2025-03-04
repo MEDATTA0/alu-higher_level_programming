0b7680aa32b4bc7d7069be58cebca3d64f6ebc4f
IBRAHIM MAÂZOU DJAHADI <132127603+MEDATTA0@users.noreply.github.com>
2025-03-04 12:59:40 +0200

    exercise 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,12 +5,16 @@ Your module is documented
 
 
 class Square:
+    """A class that defines a square."""
+
     def __init__(self, size=0):
         try:
             if isinstance(size) != int:
                 raise TypeError()
             elif size < 0:
                 raise ValueError()
+            else:
+                self.__size = size
         except TypeError:
             print("size must be an integer")
         except ValueError:
